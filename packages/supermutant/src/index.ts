833a398ec1fbf305ffa1b106520c1ebdeb731068
Hendrik Mans <hendrik@mans.de>
2023-01-21 11:56:41 +0100

    Yup

--- packages/supermutant/src/index.ts
+++ packages/supermutant/src/index.ts
@@ -2,22 +2,21 @@ import { Event } from "eventery";
 
 const callbacks = new WeakMap<any, Event<any>>();
 
-export function onUpdated<T>(store: T, callback: (store: T) => void) {
-  if (!callbacks.has(store)) {
-    callbacks.set(store, new Event());
+export function onUpdate<T>(subject: T) {
+  let event = callbacks.get(subject);
+
+  if (!event) {
+    event = new Event();
+    callbacks.set(subject, event);
   }
 
-  const event = callbacks.get(store)!;
-  event.subscribe(callback);
-  return () => event.unsubscribe(callback);
+  return event;
 }
 
-export function update<T>(store: T, callback?: (store: T) => void) {
-  if (callback) {
-    callback(store);
-  }
+export function update<T>(store: T, mutator?: (store: T) => void) {
+  /* Execute mutator if one is given */
+  mutator?.(store);
 
-  if (callbacks.has(store)) {
-    callbacks.get(store)!.emit(store);
-  }
+  /* Emit update event if one exists */
+  callbacks.get(store)?.emit(store);
 }
--- packages/supermutant/test/index.test.ts
+++ packages/supermutant/test/index.test.ts
@@ -1,7 +1,22 @@
-import { add } from "../src";
+import { onUpdate, update } from "../src";
+import { Event } from "eventery";
 
-describe(add, () => {
-  it("adds two numberts", () => {
-    expect(add(1, 2)).toBe(3);
+describe(onUpdate, () => {
+  it("returns an event object representing updates to the given subject", () => {
+    const subject = {};
+    const event = onUpdate(subject);
+
+    expect(event).toBeDefined();
+    expect(event).toBeInstanceOf(Event);
+  });
+
+  it("gets emitted when an update is performed on the subject", () => {
+    const subject = {};
+
+    const listener = jest.fn();
+    onUpdate(subject).subscribe(listener);
+
+    update(subject);
+    expect(listener).toHaveBeenCalled();
   });
 });
