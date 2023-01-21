50f7bd264af0cc2c7063ae73345dfdca1c68e51f
Hendrik Mans <hendrik@mans.de>
2023-01-21 11:45:18 +0100

    Import code from prototype

--- packages/supermutant/src/index.ts
+++ packages/supermutant/src/index.ts
@@ -1,3 +1,23 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { Event } from "eventery";
+
+const callbacks = new WeakMap<any, Event<any>>();
+
+export function onUpdated<T>(store: T, callback: (store: T) => void) {
+  if (!callbacks.has(store)) {
+    callbacks.set(store, new Event());
+  }
+
+  const event = callbacks.get(store)!;
+  event.subscribe(callback);
+  return () => event.unsubscribe(callback);
+}
+
+export function update<T>(store: T, callback?: (store: T) => void) {
+  if (callback) {
+    callback(store);
+  }
+
+  if (callbacks.has(store)) {
+    callbacks.get(store)!.emit(store);
+  }
 }
