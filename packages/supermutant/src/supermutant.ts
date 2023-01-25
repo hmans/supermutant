0535d85654b4d568f3ae618afc268b2dd511b9cb
Hendrik Mans <hendrik@mans.de>
2023-01-25 19:20:29 +0100

    Docs

--- packages/supermutant/src/supermutant.ts
+++ packages/supermutant/src/supermutant.ts
@@ -1,9 +1,22 @@
 import { Event } from "eventery";
 
+/**
+ * In Supermutant, any object can be a subject.
+ */
 export type Subject = any;
 
+/**
+ * A map of subjects to their update events.
+ */
 const callbacks = new WeakMap<Subject, Event<[subject: Subject]>>();
 
+/**
+ * Returns an event that emits when the given subject is updated
+ * through the `mutate` function.
+ *
+ * @param subject The subject to return the update event for.
+ * @returns An event that emits when the subject is updated.
+ */
 export function mutationEvent<T extends Subject>(subject: T) {
   let event = callbacks.get(subject);
 
@@ -15,6 +28,17 @@ export function mutationEvent<T extends Subject>(subject: T) {
   return event;
 }
 
+/**
+ * Subscribes to updates on the given subject. Will execute the given callback
+ * every time the subject is updated through the `mutate` function. Optionally,
+ * a selector function can be provided that selects a value from the subject. The
+ * callback will only be executed when the selected value changes.
+ *
+ * @param subject The subject to subscribe to.
+ * @param callback The callback to execute when the subject is updated.
+ * @param selector An optional function that selects a value from the subject. The callback will only be executed when the selected value changes.
+ * @returns A function that will unsubscribe the listener.
+ */
 export function onMutate<T extends Subject>(
   subject: T,
   callback: (subject: T) => void,
@@ -36,6 +60,15 @@ export function onMutate<T extends Subject>(
   }
 }
 
+/**
+ * Updates the given subject. A mutater function can be provided that will be
+ * executed before the update event is emitted, but this is purely cosmetic.
+ * Most importantly, all subscribers to the subject's update event will be
+ * notified of the update.
+ *
+ * @param subject The subject to update.
+ * @param mutator An optional function that mutates the subject.
+ */
 export function mutate<T extends Subject>(
   subject: T,
   mutator?: (store: T) => void
