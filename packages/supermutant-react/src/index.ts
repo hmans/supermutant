b586ec3a8fe5df676d0c4a68b93e53aa82e0ad59
Hendrik Mans <hendrik@mans.de>
2023-01-25 19:39:23 +0100

    Simplify hook

--- packages/supermutant-react/src/index.ts
+++ packages/supermutant-react/src/index.ts
@@ -10,27 +10,6 @@ export function useMutant<T, U>(subject: T, selector: (subject: T) => U): U;
 
 export function useMutant<T, U>(subject: T, selector?: (subject: T) => U) {
   const rerender = useRerender();
-
-  const [value, setValue] = useState(() =>
-    selector ? selector(subject) : subject
-  );
-
-  /* Let this component know when the subject is updated. */
-  useEffect(
-    () =>
-      onMutate(subject, (subject) => {
-        if (selector) {
-          const newValue = selector(subject);
-          if (newValue !== value) {
-            setValue(newValue);
-            rerender();
-          }
-        } else {
-          rerender();
-        }
-      }),
-    []
-  );
-
+  useEffect(() => onMutate(subject, rerender, selector), []);
   return selector ? selector(subject) : subject;
 }
